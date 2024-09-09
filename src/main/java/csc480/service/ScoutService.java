83dbfbc45a51def5a3256e687649bc2ae498d630
Gotoline-one <momsen.peter@gmail.com>
2024-09-08 22:38:00 -0700

    Merge pull request #5 from Gotoline-one/Gotoline-one-patch-newBadge
    updated new merit badge functionality

--- src/main/java/csc480/service/ScoutService.java
+++ src/main/java/csc480/service/ScoutService.java
@@ -2,12 +2,45 @@ package csc480.service;
 
 import csc480.model.Scout;
 import csc480.repository.ScoutRepository;
+import csc480.repository.json.JsonScoutRepo;
+import csc480.repository.mongo.MongoScoutRepo;
+
+import java.util.ArrayList;
 
 public class ScoutService {
-    ScoutRepository scoutRemoteRepo;
+    private ScoutRepository remoteRepository = new MongoScoutRepo();
+    private ScoutRepository localRepository = new JsonScoutRepo();
+    private boolean isConnected = true;
 
-    ScoutService(){
-//        scoutRemoteRepo = new ScoutRepository();
+    public void updateScout(Scout scout) {
+        try {
+            if (isConnected) {
+                remoteRepository.updateScout(scout);
+            } else {
+                localRepository.updateScout(scout);
+            }
+        } catch (Exception e) {
+            System.out.println("Connection to MongoDB failed, saving locally.");
+            localRepository.updateScout(scout);
+            isConnected = false;
+        }
+    }
+
+    public ArrayList<Scout> findAll(){
+        try {
+            if (isConnected) {
+                return remoteRepository.findAll();
+            } else {
+                return localRepository.findAll();
+            }
+        } catch (Exception e) {
+            System.out.println("Connection to MongoDB failed, saving locally.");
+            isConnected = false;
+            return localRepository.findAll();
+        }
+
+
+
     }
 
 }
